c5a2e2968e86465bf8a8b96caea1d7f2997fbb43
Munish Raj <munish02@gmail.com>
2025-02-18 01:49:05 +0530

    Trident files

--- appTestOnDesktop.py
+++ appTestOnDesktop.py
@@ -30,6 +30,18 @@ else:
 def landing():
     return render_template('landing.html')
 
+@app.route('/trident')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/ass', methods=['GET', 'POST'])
 def chat():
     global thread_store  # Ensure we're working with the global thread_store
